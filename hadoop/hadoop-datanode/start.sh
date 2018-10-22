a6d0f678e3d2e6de54ea621ca98af7c29540359b
luis.ruiz@pagosonline.com <luis.ruiz@pagosonline.com>
2018-10-22 07:32:42 -0500

    Initial date for charge modified

--- hadoop/hadoop-datanode/start.sh
+++ hadoop/hadoop-datanode/start.sh
@@ -8,7 +8,7 @@ sleep 15
 
 if ! $(hadoop fs -test -d /user/hive/warehouse/matches); then
   echo -e "Sqooping matches table into hive"
-  sqoop import --connect jdbc:postgresql://10.60.227.13:5432/maf --table matches --username maf --password MAF_uaDYjiiILdG --query "SELECT * FROM maf.maf_transaccion WHERE fecha_creacion > '2018-04-01'" --create-hive-table --hive-import -m1
+  sqoop import --connect jdbc:postgresql://10.60.227.13:5432/maf --table matches --username maf --password MAF_uaDYjiiILdG --query "SELECT * FROM maf.maf_transaccion WHERE fecha_creacion > '2018-10-20'" --create-hive-table --hive-import -m1
 fi
 
 echo -e "\e[01;32m*\e[00m `date` \e[01;32mStarted DataNode\e[00m"
