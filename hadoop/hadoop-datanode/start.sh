ee2803e8bc444ad3a8b9ad1156a4cbc70c7e0f47
luis.ruiz@pagosonline.com <luis.ruiz@pagosonline.com>
2018-10-23 10:57:29 -0500

    Import query modified

--- hadoop/hadoop-datanode/start.sh
+++ hadoop/hadoop-datanode/start.sh
@@ -6,10 +6,16 @@ echo -e "\e[01;32m*\e[00m `date` \e[01;32mWaiting on datanode to ensure namenode
 
 sleep 15
 
-if ! $(hadoop fs -test -d /user/hive/warehouse/matches); then
-  echo -e "Sqooping matches table into hive"
-  sqoop import --connect jdbc:postgresql://10.60.227.13:5432/maf --table maf.maf_device_info --username maf --password MAF_uaDYjiiILdG --create-hive-table --hive-import -m1
+if ! $(hadoop fs -test -d /user/hive/warehouse/maf_transaccion); then
+  for i in {04,05,06,07,08,09}
+  do
+    echo -e "Sqooping maf_transaccion table into hive"
+    sqoop import --connect jdbc:postgresql://10.60.227.13:5432/maf --hive-table maf_transaccion$i --target-dir "/tmp/data$i" --create-hive-table \
+    --query "SELECT * FROM maf_transaccion WHERE fecha_creacion >='2018-$i-01' AND fecha_creacion < '2018-$((i+1))-01' AND \$CONDITIONS" \
+    --username maf --password MAF_uaDYjiiILdG --hive-import -m1 --map-column-hive ids_transacciones_similares=STRING,ids_transacciones_similares_comercio=STRING
+  done
 fi
+
 
 echo -e "\e[01;32m*\e[00m `date` \e[01;32mStarted DataNode\e[00m"
 
